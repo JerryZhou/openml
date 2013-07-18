d0695c49086823fd778cf75767b5fae329269f48
JerryZhou <JerryZhou@outlook.com>
2013-07-18 17:18:44 +0800

    fix

--- jni/include/mutex.h
+++ jni/include/mutex.h
@@ -12,7 +12,7 @@ public:
 	    pthread_mutexattr_t    attr;
 	    pthread_mutexattr_init(&attr);
 	    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
-	    pthread_mutex_init(&mutex, 0);
+	    pthread_mutex_init(&mutex, &attr);
 	    pthread_mutexattr_destroy(&attr);
 	}
 	~ThreadMutex(){
